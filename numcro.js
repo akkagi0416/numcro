d125f81bb8261ca2f195a1ace09a940347660093
akkagi0416 <akkagi0416@gmail.com>
2017-08-20 01:05:16 +0900

    add inline method

--- numcro.js
+++ numcro.js
@@ -16,26 +16,38 @@ var arr_init = [
 var step = 0
 
 var auto    = document.getElementById('auto')
+var simple  = document.getElementById('simple')
 var line_h  = document.getElementById('line_h')
 var line_v  = document.getElementById('line_v')
 var box     = document.getElementById('box')
+var only1   = document.getElementById('only1')
 var only1_h = document.getElementById('only1_h')
 var only1_v = document.getElementById('only1_v')
 var only1_box = document.getElementById('only1_box')
+var pair = document.getElementById('pair')
 var pair_h = document.getElementById('pair_h')
 var pair_v = document.getElementById('pair_v')
 var pair_box = document.getElementById('pair_box')
+var inline= document.getElementById('inline')
+var inline_h = document.getElementById('inline_h')
+var inline_v = document.getElementById('inline_v')
 
 auto.addEventListener('click', function(){ func_auto() })
+simple.addEventListener('click', function(){ func_simple() })
 line_h.addEventListener('click', function(){ func_line_h() })
 line_v.addEventListener('click', function(){ func_line_v() })
 box.addEventListener('click', function(){ func_box() })
+only1.addEventListener('click', function(){ func_only1() })
 only1_h.addEventListener('click', function(){ func_only1_h() })
 only1_v.addEventListener('click', function(){ func_only1_v() })
 only1_box.addEventListener('click', function(){ func_only1_box() })
+pair.addEventListener('click', function(){ func_pair() })
 pair_h.addEventListener('click', function(){ func_pair_h() })
 pair_v.addEventListener('click', function(){ func_pair_v() })
 pair_box.addEventListener('click', function(){ func_pair_box() })
+inline.addEventListener('click', function(){ func_inline() })
+inline_h.addEventListener('click', function(){ func_inline_h() })
+inline_v.addEventListener('click', function(){ func_inline_v() })
 
 function initialize(){
   for(var y = 0; y < 9; y++){
@@ -151,7 +163,7 @@ function debug_step(){
   arr_display(DEBUG)
 }
 
-function step1(){
+function func_simple(){
   func_line_h()
   func_line_v()
   func_box()
@@ -163,20 +175,27 @@ function func_auto(){
   while(1){
     length_last = arr_length()
 
-    // step1
-    step1()
+    // simple
+    func_simple()
+
+    // only1
+    func_only1()
+
+    // pair
+    func_pair()
 
     length_new = arr_length()
 
-    if(length_last == length_new){
-      // step2
-      func_only1_h()
-      func_only1_v()
-      func_only1_box()
-
-      length_new = arr_length()
-      if(length_last == length_new){ break }
-    }
+    if(length_last == length_new){ break }
+    // if(length_last == length_new){
+    //   // step2
+    //   func_only1_h()
+    //   func_only1_v()
+    //   func_only1_box()
+    //
+    //   length_new = arr_length()
+    //   if(length_last == length_new){ break }
+    // }
   }
 }
 
@@ -232,6 +251,12 @@ function func_box(){
     } // box_x
   } // box_y
   arr_display(DEBUG, 'box')
+}
+
+function func_only1(){
+  func_only1_h()
+  func_only1_v()
+  func_only1_box()
 }
 
 function func_only1_h(){
@@ -318,6 +343,12 @@ function func_only1_box(){
   arr_display(DEBUG, 'only1_box')
 }
 
+function func_pair(){
+  func_pair_h()
+  func_pair_v()
+  func_pair_box()
+}
+
 function func_pair_h(){
   for(var y = 0; y < 9; y++){
     for(var x = 0; x < 9; x++){
@@ -387,7 +418,7 @@ function func_pair_box(){
                       arr[box_y * 3 + j][box_x * 3 + i] = arr[box_y * 3 + j][box_x * 3 + i].replace(cell.charAt(1), '')
                     }
                   }
-                  continue box_next // pariはboxに1つなはずなので、1つ処理したら次のboxへ
+                  continue box_next // pairはboxに1つなはずなので、1つ処理したら次のboxへ
                 }
               }
             }
@@ -397,6 +428,103 @@ function func_pair_box(){
     }
   }
   arr_display(DEBUG, 'pair_box')
+}
+
+function func_inline(){
+  func_inline_h()
+  func_inline_v()
+}
+function func_inline_h(){
+  console.log('h')
+
+  for(var box_y = 0; box_y < 3; box_y++){
+    for(var box_x = 0; box_x < 3; box_x++){
+      // box内で未決定が2 or 3個の数字を収集
+      var number_all_in_box = ''
+      for(var y = 0; y < 3; y++){
+        for(var x = 0; x < 3; x++){
+          number_all_in_box += arr[box_y * 3 + y][box_x * 3 + x]
+        }
+      }
+      var tmp_2 = pickup_2char(number_all_in_box)
+      var tmp_3 = pickup_3char(number_all_in_box)
+      // console.log("box[" + box_y + "][" + box_x + "]:" + number_all_in_box + " -> 2:" + tmp_2 + " 3:" + tmp_3)
+      var char_2or3 = tmp_2 + tmp_3
+
+      // 2 or 3文字が1列なら他boxの候補を削除
+      if(char_2or3.length != 0){
+        for(var l = 0; l < char_2or3.length; l++){
+          var collect_y_corrdinate = ''
+          for(var y = 0; y < 3; y++){
+            for(var x = 0; x < 3; x++){
+              if(arr[box_y * 3 + y][box_x * 3 + x].indexOf(char_2or3[l]) != -1){
+                collect_y_corrdinate += box_y * 3 + y
+              }
+            }
+          }
+          // console.log("box[" + box_y + "][" + box_x + "]:" + char_2or3[l] + "->" + collect_y_corrdinate + " " + is_same(collect_y_corrdinate))
+          if(is_same(collect_y_corrdinate)){
+            var char_delete  = char_2or3[l]
+            var y_corrdinate = collect_y_corrdinate[0]
+            for(var box_i = 0; box_i < 3; box_i++){
+              if(box_x == box_i){ continue }
+              for(var i = 0; i < 3; i++){
+                arr[y_corrdinate][box_i * 3 + i] = arr[y_corrdinate][box_i * 3 + i].replace(char_delete, '')
+                console.log("box[" + box_y + "][" + box_x + "]:" + char_2or3[l] + "->y_corrdinate " + collect_y_corrdinate)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  arr_display(DEBUG, 'inline_h')
+}
+function func_inline_v(){
+  console.log('v')
+
+  for(var box_y = 0; box_y < 3; box_y++){
+    for(var box_x = 0; box_x < 3; box_x++){
+      // box内で未決定が2 or 3個の数字を収集
+      var number_all_in_box = ''
+      for(var y = 0; y < 3; y++){
+        for(var x = 0; x < 3; x++){
+          number_all_in_box += arr[box_y * 3 + y][box_x * 3 + x]
+        }
+      }
+      var tmp_2 = pickup_2char(number_all_in_box)
+      var tmp_3 = pickup_3char(number_all_in_box)
+      // console.log("box[" + box_y + "][" + box_x + "]:" + number_all_in_box + " -> 2:" + tmp_2 + " 3:" + tmp_3)
+      var char_2or3 = tmp_2 + tmp_3
+
+      // 2 or 3文字が1列なら他boxの候補を削除
+      if(char_2or3.length != 0){
+        for(var l = 0; l < char_2or3.length; l++){
+          var collect_x_corrdinate = ''
+          for(var y = 0; y < 3; y++){
+            for(var x = 0; x < 3; x++){
+              if(arr[box_y * 3 + y][box_x * 3 + x].indexOf(char_2or3[l]) != -1){
+                collect_x_corrdinate += box_x * 3 + x
+              }
+            }
+          }
+          // console.log("box[" + box_y + "][" + box_x + "]:" + char_2or3[l] + "->" + collect_x_corrdinate + " " + is_same(collect_x_corrdinate))
+          if(is_same(collect_x_corrdinate)){
+            var char_delete  = char_2or3[l]
+            var x_corrdinate = collect_x_corrdinate[0]
+            for(var box_j = 0; box_j < 3; box_j++){
+              if(box_y == box_j){ continue }
+              for(var j = 0; j < 3; j++){
+                arr[box_j * 3 + j][x_corrdinate] = arr[box_j * 3 + j][x_corrdinate].replace(char_delete, '')
+                console.log("box[" + box_y + "][" + box_x + "]:" + char_2or3[l] + "->x_corrdinate " + collect_x_corrdinate)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  arr_display(DEBUG, 'inline_v')
 }
 
 // '11232' -> '2'
@@ -409,6 +537,26 @@ function pickup_1char(str){
     }
   }
   return str_1char
+}
+function pickup_2char(str){
+  var str_2char = ''
+  for(var i = 1; i < 10; i++){
+    var str_tmp = str.replace(new RegExp(i, 'g'), '')
+    if(str.length - str_tmp.length == 2){ str_2char += i }
+  }
+  return str_2char
+}
+function pickup_3char(str){
+  var str_3char = ''
+  for(var i = 1; i < 10; i++){
+    var str_tmp = str.replace(new RegExp(i, 'g'), '')
+    if(str.length - str_tmp.length == 3){ str_3char += i }
+  }
+  return str_3char
+}
+function is_same(str){
+  var tmp = str.replace(new RegExp(str[0], 'g'), '')
+  return tmp.length == 0 ? true : false
 }
 
 initialize()
